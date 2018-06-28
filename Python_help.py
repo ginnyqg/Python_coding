6bb87168ca2e4b1cb576c7cd6bcbcf9917d40192
Ginny Gao <qinqingao1a@hotmail.com>
2018-06-28 14:10:52 -0500

    Update Python_help.py

--- Python_help.py
+++ Python_help.py
@@ -331,6 +331,70 @@ plt.rc('legend', fontsize = SMALL_SIZE)    # legend fontsize
 
 
 fig.savefig(filepath + str(plot_title) + '.png')      
+         
+          
+          
+          
+          
+#loop, print
+#plot delta pressure figures for when pumps fail
+
+well_name = ['a', 'b', 'c', 'd', 'e']
+
+#custom size
+SMALL_SIZE = 16
+MEDIUM_SIZE = 20
+BIGGER_SIZE = 25
+
+plt.rc('font', size = MEDIUM_SIZE)          # controls default text sizes
+plt.rc('axes', titlesize = BIGGER_SIZE)     # fontsize of the axes title
+plt.rc('axes', labelsize = MEDIUM_SIZE)    # fontsize of the x and y labels
+plt.rc('xtick', labelsize = MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('ytick', labelsize = MEDIUM_SIZE)    # fontsize of the tick labels
+plt.rc('legend', fontsize = MEDIUM_SIZE)    # legend fontsize
+
+#following lists need to be same length, for each index of each list, column needs to be same length
+x = [col1['tstamp'], col2['tstamp'], col3['tstamp'], col4['tstamp'], col5['tstamp']]
+y1 = [col1['abc'], col2['abc'], col3['abc'], col4['abc'], col5['abc']]
+y2 = [col6['def'], col7['def'], col8['def'], col9['def'], col10['def']]
 
 
+for i in range(len(x)):
+    plt.figure()
+    # plot
+    fig = plt.plot(x[i], y1[i] - y2[i], linestyle = 'None', marker = 'o')
 
+    fig = plt.gcf()
+
+    # format the ticks
+    fig.autofmt_xdate()
+    xmin, xmax = plt.gca().get_xlim()
+    plt.gca().set_xticks(np.round(np.linspace(xmin, xmax, 12), 2))
+
+
+    #make legend
+    fig.legend(['ghi'])
+
+
+    #increase figure size
+    fig.set_size_inches(25, 15)
+
+
+    #add title for y axis, figure
+    plot_ylabel = 'ijk)'
+    plot_title = 'Failure for ' + str(well_name[i]) + ' wrt ' + str(plot_ylabel)
+
+
+    plt.title(str(plot_title), fontsize = 35)
+    plt.ylabel(plot_ylabel)
+    
+#     fig.savefig(filepath + str(plot_title) + '.png')
+          
+
+#check column type
+colA.dtype
+
+          
+          
+          
+          
