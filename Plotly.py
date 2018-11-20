968c2ce91e98e0b2b44bcc9b9984d4a673e8d5f6
Ginny Gao <qinqingao1a@hotmail.com>
2018-11-20 12:15:33 -0500

    Create Plotly.py

---
+++ Plotly.py
@@ -0,0 +1,14 @@
+import plotly
+plotly.tools.set_credentials_file(username = '', api_key = '')
+
+import plotly.dashboard_objs as dashboard
+import IPython.display
+from IPython.display import Image
+ 
+my_dboard = dashboard.Dashboard()
+my_dboard.get_preview()
+
+
+
+
+
