39969ed3fb872221955f0b623398fee9b8b31ed8
Ginny Gao <qinqingao1a@hotmail.com>
2019-02-28 22:09:14 -0600

    Update Python_help.py

--- Python_help.py
+++ Python_help.py
@@ -230,7 +230,38 @@ lats, lons = [],[]
 for index, row in df_select.iterrows():
     lats.append(float(df_select['LAT']))
     lons.append(float(df_select['LONG']))
-              
+
+	  
+# How much to zoom from coordinates (in degrees)
+zoom_scale = 0
+
+# Setup the bounding box for the zoom and bounds of the map
+bbox = [np.min(lats) - zoom_scale, np.max(lats) + zoom_scale, \
+        np.min(lons) - zoom_scale, np.max(lons) + zoom_scale]
+
+plt.figure(figsize = (10, 10))
+# Define the projection, scale, the corners of the map, and the resolution.
+m = Basemap(projection = 'merc', llcrnrlat = bbox[0], urcrnrlat = bbox[1], \
+            llcrnrlon = bbox[2], urcrnrlon = bbox[3], lat_ts = 10, resolution = 'i')
+
+# Draw coastlines and fill continents and water with color
+m.drawcoastlines()
+m.fillcontinents(color = 'peru', lake_color = 'dodgerblue')
+
+# draw parallels, meridians, and color boundaries
+m.drawparallels(np.arange(bbox[0], bbox[1], (bbox[1] - bbox[0]) / 5), labels = [1, 0, 0, 0])
+m.drawmeridians(np.arange(bbox[2], bbox[3], (bbox[3] - bbox[2]) / 5), labels = [0, 0, 0, 1], rotation = 45)
+m.drawmapboundary(fill_color = 'dodgerblue')
+
+# build and plot coordinates onto map
+x,y = m(lons, lats)
+m.plot(x, y, 'r*', markersize = 5)
+plt.title("Geo grid")
+# plt.savefig('geo_grid.png', format='png', dpi=500)
+plt.show()
+	  
+	  
+	  
           
 #python index of these rows whose columns meet certain conditions
 dataset2 = dataset1.loc[(dataset1['col1'] == 'abc') | 
