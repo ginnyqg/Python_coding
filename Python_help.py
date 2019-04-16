9b03cf073c56c3c3818b4d2cd17390ebfdd1ed57
Ginny Gao <qinqingao1a@hotmail.com>
2019-04-15 21:47:35 -0500

    Update Python_help.py

--- Python_help.py
+++ Python_help.py
@@ -1419,6 +1419,22 @@ print('Took {} Seconds'.format(t2 - t1))
 schedule.every(10).minutes.do(myfunc)	 
 		 
 		 
+#module: file contain python code; can be executed or imported
+# https://www.youtube.com/watch?v=NmiwZ4y1iiM
+# from module import variables	 
+
+#class: python code
+class Classname:
+    pass
+
+
+
+
+		 
+		 
+		 
+		 
+		 
 		 
 		 
 		 
